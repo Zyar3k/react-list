007d926a0e6bab6499ae5b12b496655e0928dd32
zyar3k <a.zygan@gmail.com>
2021-10-05 18:41:12 +0200

    Add adding new car to list

--- src/App.js
+++ src/App.js
@@ -6,34 +6,78 @@ const data = [
   { id: "2", brand: "Mitsubishi", model: "Lancer", year: 1980 },
   { id: "3", brand: "Subaru", model: "Impreza", year: 1990 },
 ];
-console.log("🚀 ~ file: App.js ~ line 9 ~ data", data);
 
 function App() {
   const [cars, setCars] = useState(data);
+  const [brand, setBrand] = useState("");
+  const [model, setModel] = useState("");
+  const [year, setYear] = useState("");
+  const [isEdit, setIsEdit] = useState(false);
 
   const handleDeleteCar = (e) => {
     const id = e.target.id;
-    console.log(typeof id);
     setCars((prev) => prev.filter((car) => car.id !== id));
   };
 
+  console.log(model);
+
+  const handleOnChangeBrand = (event) => setBrand(event.target.value);
+  const handleOnChangeModel = (event) => setModel(event.target.value);
+  const handleOnChangeYear = (event) => setYear(event.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const basicId = Math.floor(Math.random() * 9999);
+    const carObject = {
+      id: basicId,
+      brand: brand,
+      model: model,
+      year: year,
+    };
+
+    const data = [...cars, carObject];
+    setCars(data);
+  };
   return (
     <div className="App">
       <h1>Cars</h1>
-      <div>
-        <label>
-          Brand
-          <input type="text" placeholder="brand" />
-        </label>
-        <label>
-          Model
-          <input type="text" placeholder="model" />
-        </label>
-        <label>
-          Year
-          <input type="text" placeholder="year" />
-        </label>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            Brand
+            <input
+              type="text"
+              placeholder="brand"
+              value={brand}
+              onChange={handleOnChangeBrand}
+            />
+          </label>
+          <label>
+            Model
+            <input
+              type="text"
+              placeholder="model"
+              value={model}
+              onChange={handleOnChangeModel}
+            />
+          </label>
+          <label>
+            Year
+            <input
+              type="text"
+              placeholder="year"
+              value={year}
+              onChange={handleOnChangeYear}
+            />
+          </label>
+        </div>
+        {isEdit ? (
+          <button type="button">Edit</button>
+        ) : (
+          <button type="submit">Add</button>
+        )}
+        <button type="button">Cancel</button>
+      </form>
       <table width="100%">
         <thead>
           <tr>
